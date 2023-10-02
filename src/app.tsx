e49e47920529bd11eee399d8344d29f04bbcce12
Alice Marshall <marshall.alice92@gmail.com>
2023-10-02 18:40:55 +0100

    Fix React createRoot warning

--- src/app.tsx
+++ src/app.tsx
@@ -3,31 +3,6 @@ import Home from "./Home";
 
 import './base.scss';
 
-const App = () => {
-    // FROM A DISASTER INTERVIEW
-
-    // const data = [{
-    //     name: "a", editor: 'vscode'
-    // }, {
-    //     name: "b", editor: 'vscode'
-    // }, {
-    //     name: "c", editor: 'unity'
-    // }];
-
-    // const total = data.length;
-
-    // const editorCount = data.reduce((obj: any, v) => {
-    //     obj[v.editor] = (obj[v.editor] || 0) + 1
-    //     return obj;
-    // }, {});
-
-    // Object.keys(editorCount).forEach((item) => {
-    //     editorCount[item] = `${((editorCount[item] / total) * 100).toFixed(1)}%`
-    // });
-
-    // console.log('editorCount', editorCount);
-
-    return <Home />;
-};
+const App = () => <Home />;
 
 export default App;

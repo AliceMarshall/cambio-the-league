9e12c7ea81ecfe8fdcb54d92b2252c4cf4b3f93a
Alice Marshall <marshall.alice92@gmail.com>
2023-10-02 19:08:51 +0100

    Updating build scripts

---
+++ webpack.production.config.js
@@ -0,0 +1,74 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = () => {
+    return {
+        entry: "./index.tsx",
+        mode: "production",
+        output: {
+            filename: "bundle.js",
+            path: path.resolve(__dirname, "dist"),
+        },
+        target: "web",
+        devServer: {
+            port: "5000",
+            static: {
+                directory: path.join(__dirname, "public"),
+            },
+            open: true,
+            hot: true,
+            liveReload: true,
+        },
+        resolve: {
+            extensions: [
+                ".js",
+                ".jsx",
+                ".json",
+                ".tsx",
+                ".ts",
+                ".scss",
+                ".css",
+            ],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: "babel-loader",
+                },
+                {
+                    test: /\.(ts|tsx)$/,
+                    exclude: /node_modules/,
+                    use: "ts-loader",
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: "style-loader",
+                        },
+                        {
+                            loader: "css-loader",
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "public", "index.html"),
+            }),
+        ],
+    };
+};
